716349b9766dc9d5add2719d3527fa60be7f64fa
jw <825140814@qq.com>
2022-02-14 00:18:52 +0800

    first commit

---
+++ src/main/java/com/test/PackageTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * @author jw
+ * @description
+ * @date 2022/2/13 21:59
+ **/
+public class PackageTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
